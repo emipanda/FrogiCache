b9834a2688c7ec1d763c8f35d8c20be7022bbabf
Emily <emilyzboro@cs.colman.ac.il>
2022-09-22 20:07:22 +0300

    first commit

---
+++ src/CachedValueStore.java
@@ -0,0 +1,93 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class CachedValueStore implements ValueStore {
+
+    // a list of all available value stores. list order should be kept.
+     private List<ValueStore> valueStores;
+     private int maxCachedItems;
+    private final Map<String, String> cachedDataMap = new HashMap<>();
+    private final LinkedList<String> cacheOrder = new LinkedList<>();
+
+
+    /**
+     * Constructor for the cache value store.
+     * @param valueStore        list of ordered value stores to operate on
+     * @param maxCachedItems    number of maximum items the cache can hold
+     */
+    public CachedValueStore(List<ValueStore> valueStore, int maxCachedItems) {
+        this.valueStores = valueStore;
+        this.maxCachedItems = maxCachedItems;
+    }
+
+    /**
+     * Iterate on valueStores and find the first key that matches. order is important.
+     */
+    public String read(String key) {
+        String cacheResult = cachedDataMap.get(key);
+        if(cacheResult == null){
+            for (ValueStore valueStore : valueStores) {
+                String result = valueStore.read(key);
+                if (valueStore.read(key) == null)
+                    continue;
+
+                //update cache key list, move value to top
+                cacheOrder.remove(key);
+                cacheOrder.addFirst(key);
+                return result;
+            }
+        }
+
+        return cacheResult;
+
+    }
+
+    /**
+     * Put <key, value> in first valueStores only. order is important.
+     */
+    public void put(String key, String value) {
+        if(cacheOrder.size() == maxCachedItems){
+            //if cache is full, remove last element from linkedList and from HashMap
+            String keyRemoved = cacheOrder.removeLast();
+            cachedDataMap.remove(keyRemoved);
+        }
+
+        // add new cache on top of the list
+        cacheOrder.addFirst(key);
+        cachedDataMap.put(key, value);
+
+        //add value to first valueStore
+        if (!valueStores.isEmpty()){
+            valueStores.get(0).put(key, value);
+        }
+        else
+            throw new IllegalStateException("valueStores is empty");
+    }
+
+    /**
+     * Iterate on valueStores and delete the key from all of them.
+     */
+    public void delete(String key) {
+        cacheOrder.remove(key);
+        cachedDataMap.remove(key);
+        valueStores.forEach(valueStore -> valueStore.delete(key));
+    }
+
+    public List<ValueStore> getValueStores() {
+        return valueStores;
+    }
+
+    public void setValueStores(List<ValueStore> valueStores) {
+        this.valueStores = valueStores;
+    }
+
+    public int getMaxCachedItems() {
+        return maxCachedItems;
+    }
+
+    public void setMaxCachedItems(int maxCachedItems) {
+        this.maxCachedItems = maxCachedItems;
+    }
+}
---
+++ src/CachedValueStoreTest.java
@@ -0,0 +1,71 @@
+/**
+ * This is a test, designed to help if needed to show the idea behind the cache.
+ * You can use it if you want
+ */
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class CachedValueStoreTest {
+
+    @Test
+    void testCache() {
+        ValueStore[] stores = new ValueStore[]{new MapValueStore()};
+        CachedValueStore cache = new CachedValueStore(List.of(stores), 5);
+        cache.put("a", "b");
+        String a = cache.read("a");
+        assertEquals("b", a);
+        cache.delete("a");
+        a = cache.read("a");
+        assertNull(a);
+    }
+
+    @Test
+    void testPut() throws IOException {
+        File inputFile = Files.createTempFile("a", ".txt").toFile();
+        inputFile.deleteOnExit();
+        ValueStore fileStore = new FileValueStore(inputFile);
+        MapValueStore mapStore = new MapValueStore();
+
+        ValueStore[] stores = new ValueStore[]{mapStore,fileStore};
+
+        CachedValueStore cache = new CachedValueStore(List.of(stores), 2);
+        cache.put("1", "a");
+        cache.put("2", "b");
+        mapStore.delete("2");
+        fileStore.delete("3");
+        cache.put("3", "c");
+        cache.put("5", "b");
+        cache.put("7", "b");
+        cache.put("2", "b");
+        cache.put("6", "b");
+
+        String a = cache.read("2");
+        assertNotNull(a);
+    }
+
+    @Test
+    void testLru() throws IOException {
+        File inputFile = Files.createTempFile("a", ".txt").toFile();
+        inputFile.deleteOnExit();
+        ValueStore fileStore = new FileValueStore(inputFile);
+        ValueStore[] stores = new ValueStore[]{fileStore};
+        CachedValueStore cache = new CachedValueStore(List.of(stores), 2);
+        cache.put("1", "a");
+        cache.put("2", "b");
+        cache.put("3", "c");
+        fileStore.delete("1");
+
+        String a = cache.read("1");
+        assertNull(a);
+    }
+
+}
